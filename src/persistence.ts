d68ca9683bd1b88e33612e8cb61c27c93fce6632
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-01-19 19:56:15 -0200

    :sparkles: add comment docs & improve readability

--- src/persistence.ts
+++ src/persistence.ts
@@ -1,15 +1,49 @@
+/**
+ * Persistence options to set timeout, storage and placeholder value.
+ */
 export interface PersistenceOptions <T = any> {
   storage?: Storage;
   timeout?: number;
   placeholder?: T;
 }
 
+/**
+ * An object to easily get and set values to a persistent storages like
+ * `SessionStorage` and `LocalStorage`.
+ */
 export interface Persistence <T = any, U = (T | undefined)> {
+  /**
+   * Set a value to a persistent storage.
+   * @param value - Any JSON parseable value can be setted.
+   */
   set (value: T): void;
+
+  /**
+   * Get value from persistent storage.
+   */
   get (): T | U;
+
+  /**
+   * Delete value from persistent storage.
+   */
   delete (): void;
 }
 
+/**
+ * Creates a Persistence, an object to easily get and set values to a persistent
+ * storage like `SessionStorage` or `LocalStorage`.
+ * @example ```js
+ * const state = createPersistence('state@name', {
+ *   placeholder: 'Unknown'
+ * });
+ *
+ * // This creates a persistent field's value.
+ * const el = document.querySelector('input[name="name"]');
+ * el.value = state.get();
+ * el.addEventListener('input', () => state.set(el.value));```
+ * @param name - Persistence uses the name as storage's key.
+ * @param options - Options to set timeout, storage and placeholder value.
+ */
 const createPersistence: {
   <T = any, U = T> (
     name: string,
@@ -20,33 +54,37 @@ const createPersistence: {
     name: string,
     options?: PersistenceOptions<U>,
   ): Persistence<T, U>;
-} = (
-  name: string, {
+} = (name: string, options: PersistenceOptions = {}): Persistence => {
+  const {
     storage = window.localStorage,
     timeout,
     placeholder,
-  }: PersistenceOptions = {}
-): Persistence => ({
-  set (value) {
-    const state = JSON.stringify({
-      value,
-      updatedAt: Date.now()
-    });
-    storage.setItem(name, state);
-  },
+  } = options;
 
-  get () {
-    const state = storage.getItem(name);
-    if (state === null)
-      return placeholder;
-    const { updatedAt, value } = JSON.parse(state);
-    const isExpired = timeout !== undefined && Date.now() > (updatedAt + timeout);
-    return isExpired ? placeholder : value;
-  },
+  const isExpired = (updatedAt: number): boolean => (
+    timeout !== undefined &&
+    Date.now() > (updatedAt + timeout)
+  );
 
-  delete () {
-    storage.removeItem(name);
-  }
-});
+  return {
+    set: (value) => {
+      const state = JSON.stringify({
+        value,
+        updatedAt: Date.now()
+      });
+      storage.setItem(name, state);
+    },
+
+    get: () => {
+      const state = storage.getItem(name);
+      if (state === null)
+        return placeholder;
+      const register = JSON.parse(state) as { updatedAt: number, value: any };
+      return isExpired(register.updatedAt) ? placeholder : register.value;
+    },
+
+    delete: () => storage.removeItem(name)
+  };
+};
 
 export default createPersistence;
