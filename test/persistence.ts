7f82b785f729c244feec9cd5de4242b5e292516e
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-01-20 05:03:45 -0200

    :white_check_mark: create unit tests for get, set and storage changes

--- test/persistence.ts
+++ test/persistence.ts
@@ -1,5 +1,5 @@
 import createPersistence, { Persistence } from '../';
-import test from 'ava';
+import test, { beforeEach } from 'ava';
 
 const storage = {
   _state: {} as Record<string, string>,
@@ -12,21 +12,70 @@ const storage = {
   },
   removeItem (key: string): void {
     delete this._state[key];
+  },
+  clear (): void {
+    this._state = {};
   }
-}
+};
+
+beforeEach(() => storage.clear());
 
 test('API: module default exports a function', (context) => {
   context.is(typeof createPersistence, 'function');
 });
 
 test('API: animate returns Animation', (context) => {
-  const persistence: Persistence<number> = createPersistence('name', {
-    storage: storage
-  });
+  const persistence: Persistence<number> = createPersistence('name', { storage });
 
   context.truthy(persistence);
   context.is(typeof persistence, 'object');
   context.is(typeof persistence.get, 'function');
   context.is(typeof persistence.set, 'function');
   context.is(typeof persistence.delete, 'function');
+});
+
+test('Persistence: Uses key as namespace to storage value', (context) => {
+  const key = 'value';
+
+  const persistence: Persistence<number> = createPersistence(key, { storage });
+
+  persistence.set(12);
+
+  context.is(typeof storage._state[key], 'string');
+
+  const state = JSON.parse(storage._state[key]);
+
+  context.is(state.value, 12);
+
+  persistence.delete();
+
+  context.is(storage._state[key], undefined);
+});
+
+test('Persistence: get() returns value from storage', (context) => {
+  const key = 'value';
+
+  storage._state[key] = `{ "value": 10000 }`;
+
+  const persistence: Persistence<number> = createPersistence(key, { storage });
+
+  context.is(persistence.get(), 10000);
+
+  persistence.set(20000);
+
+  context.is(persistence.get(), 20000);
+});
+
+test('Persistence: set() saves value to storage', (context) => {
+  const key = 'value';
+
+  const persistence: Persistence<number> = createPersistence(key, { storage });
+
+  persistence.set(12);
+
+  context.is(persistence.get(), 12);
+
+  persistence.set(50); // Overwrites value.
+
+  context.is(persistence.get(), 50);
 });
